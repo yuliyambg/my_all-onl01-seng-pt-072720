dcf2eea47f33768a32ba09e93ba1eaaafce08a18
secure-revert-8990 <yuliyambg@gmail.com>
2020-07-30 17:28:28 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,9 +1,10 @@
 require 'pry'
 
 def my_all?(collection)
-i = 0
-while i < collection.length
-yield(collection[i])
-i+=1
- end 
+  i = 0
+  block_return_values = []
+  while i < collection.length
+    block_return_values << yield(collection[i])
+    i = i + 1
+  end
 end
